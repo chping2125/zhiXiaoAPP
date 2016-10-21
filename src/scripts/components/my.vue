95d41bac9a334a6d6809fd7183682d08ce77c82c
chping2125 <chping_2125@163.com>
2016-10-21 23:44:46 +0800

    my.vue basic success

--- src/scripts/components/my.vue
+++ src/scripts/components/my.vue
@@ -1,3 +1,113 @@
 <template>
-  my...
+  <div class="my-container">
+    <div class="header">
+      <span>我的</span>
+    </div>
+    <div class="content">
+      <div>
+        <div class="first one">
+          <div class="left">
+            <div class=""><img src="http://placehold.it/48x48" alt="" /></div>
+            <span>未登录</span>
+          </div>
+          <div class="right">
+            <button>登陆/注册</button>
+          </div>
+        </div>
+        <div class="two">
+          <div class="left">
+            <span><img src="/images/my/user_invite.png" alt="" /></span>
+            <i>邀请好友</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="first">
+          <div class="left">
+            <span><img src="/images/my/user_news.png" alt="" /></span>
+            <i>我的消息</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div  class="two">
+          <div class="left">
+            <span><img src="/images/my/user_collect.png" alt="" /></span>
+            <i>我的收藏</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div class="three">
+          <div class="left">
+            <span><img src="/images/my/user_history.png" alt="" /></span>
+            <i>阅读历史</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="first">
+          <div class="left">
+            <span><img src="/images/my/user_task.png" alt="" /></span>
+            <i>任务中心</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div  class="two">
+          <div class="left">
+            <span><img src="/images/my/user_exchange.png" alt="" /></span>
+            <i>兑换中心</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div class="three">
+          <div class="left">
+            <span><img src="/images/my/user_income.png" alt="" /></span>
+            <i>收支明细</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="first">
+          <div class="left">
+            <span><img src="/images/my/user_feedback.png" alt="" /></span>
+            <i>有奖反馈</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div  class="two">
+          <div class="left">
+            <span><img src="/images/my/user_setting.png" alt="" /></span>
+            <i>设置</i>
+          </div>
+          <div class="right">
+            <span></span>
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script type="text/javascript">
+
+</script>
