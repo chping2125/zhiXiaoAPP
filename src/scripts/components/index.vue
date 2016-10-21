1241c6738211e561e11d93409fbf7fc93345d8ff
chping2125 <chping_2125@163.com>
2016-10-21 14:25:25 +0800

    vupdate iew -footerr

--- src/scripts/components/index.vue
+++ src/scripts/components/index.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>
+  <div class="box">
     <div class="index-container">
 
     </div>
     <footer id="footer">
-      footer
+      <ul>
+        <!-- <li :class="{{'active':one}}"> -->
+        <li class="active">
+          <i class="iconfont">&#xe6bb;</i>
+          <p>主页</p>
+        </li>
+        <!-- <li :class="{{'active':two}}"> -->
+        <li>
+          <i class="iconfont">&#xe664;</i>
+          <p>排行榜</p>
+        </li>
+        <!-- <li :class="{{'active':three}}"> -->
+        <li>
+          <i class="iconfont">&#xe65c;</i>
+          <p>发现</p>
+        </li>
+        <!-- <li :class="{{'active':four}}"> -->
+        <li>
+          <i class="iconfont">&#xe735;</i>
+          <p>我的</p>
+        </li>
+      </ul>
     </footer>
   </div>
 </template>
 
 <script>
-  console.log(1111)
+  // var Vue = require('../libs/vue.js');
+  // var VueResource = require('../libs/vue-resource.js');
+  // Vue.use(VueResource);
+  //
+  // var vm = new Vue({
+  //   el:'#footer',
+  //   data:{
+  //     one: true,
+  //     two: false,
+  //     three: false,
+  //     four: false
+  //   }
+  // });
 </script>
