511ccd99f7d6854874d2e69ebbd1cbc654d49c3f
chping2125 <chping_2125@163.com>
2016-10-25 22:16:45 +0800

    chping branch merge

--- prd/mock/mock.js
+++ prd/mock/mock.js
@@ -1,11 +1,13 @@
 var user = require('./my/user.js');
 var vali = require('./my/vali.js');
 var username = require('./my/username.js');
+var password = require('./my/password.js');
 
 module.exports = function() {
     return {
         "user": user,
         "vali": vali,
-        "username":username
+        "username": username,
+        "password": password
     }
 }
