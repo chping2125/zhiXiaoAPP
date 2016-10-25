bddc5cd53eaa1b6acdf5a3006aa55418abc808b2
lilyone <804654484@@qq.com>
2016-10-25 22:12:39 +0800

    error

--- prd/mock/mock.js
+++ prd/mock/mock.js
@@ -1,11 +1,17 @@
 var user = require('./my/user.js');
 var vali = require('./my/vali.js');
 var username = require('./my/username.js');
+var hot = require('./lee/hot.js');
+var hotRefresh = require('./lee/refresh.js');
+var hotUsers = require('./lee/users.js');
 
 module.exports = function() {
     return {
         "user": user,
         "vali": vali,
-        "username":username
+        "username":username,
+        "hot": hot,
+        "hotRefresh":hotRefresh,
+        "hotUsers":hotUsers
     }
 }
